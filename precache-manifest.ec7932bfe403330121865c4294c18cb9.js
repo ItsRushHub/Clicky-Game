5ad54eda448af749130a353a04457b027d41f092
Oran Rush <oranrush@Orans-MacBook-Pro.local>
2019-04-27 12:10:46 -0500

    Updates

---
+++ precache-manifest.ec7932bfe403330121865c4294c18cb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a38dcfb980206580ff6482b6be040baf",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "d4dd620adc5f9bfc69fc",
+    "url": "/clicky-game/static/css/main.227a60dc.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "d4dd620adc5f9bfc69fc",
+    "url": "/clicky-game/static/js/main.302f313b.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "41e316ca67a66099859022bedd4aa340",
+    "url": "/clicky-game/static/media/oscar-de-la-hoya-net-worth2-300x437.41e316ca.jpg"
+  }
+]);
